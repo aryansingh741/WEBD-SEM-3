5851fd780e1d4d611ff35fb170c6de901215bea2
aryansingh741 <aryansingh69876@gmail.com>
2026-09-09 10:17:56 +0530

    first commit

---
+++ indx.jsx
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JavaScript Example</title>
+</head>
+<body>
+
+<h2>JavaScript Addition Program</h2>
+
+<script>
+    let a = 15;
+    let b = 25;
+    let sum = a + b;
+
+    document.write("Sum = " + sum);
+</script>
+
+</body>
+</html>  
